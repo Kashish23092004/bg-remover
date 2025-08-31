7a2901b3dbba309a5f22bdbccf21392ec195a917
Kashish Singh <kashish2392004@gmail.com>
2025-08-31 20:09:53 +0530

    change mongodb

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
-const connectDB = async()=>{
-    mongoose.connection.on('connected',()=>{
-        console.log('mongodb connected')
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/Imagebgremover`)
-}
+const connectDB = async () => {
+  mongoose.connection.on('connected', () => {
+    console.log('mongodb connected');
+  });
+  await mongoose.connect(`${process.env.MONGODB_URI}/Imagebgremover`);
+};
+
 export default connectDB;
